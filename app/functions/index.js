4fae0142443bff20f8f0d752f31ad683f21095bd
kp1129 <katya.p.1129@gmail.com>
2020-12-10 16:04:11 -0700

    add firebase cloud functions backend - setup

---
+++ app/functions/index.js
@@ -0,0 +1,5 @@
+const dotenv = require('dotenv').config();
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const stripe = require('stripe')();
